7ed2ed9606e426d5e3a35f298219bad42a057a2b
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-10-22 19:12:41 +0200

    feat: flow of code completed

---
+++ app/src/main/java/edu/iesam/meceiot/features/lorawan/data/local/LoraWanXmlLocalDataSource.kt
@@ -0,0 +1,4 @@
+package edu.iesam.meceiot.features.lorawan.data.local
+
+class LoraWanXmlLocalDataSource {
+}
--- app/src/main/java/edu/iesam/meceiot/features/lorawan/presentation/LoraWanAboutFragment.kt
+++ app/src/main/java/edu/iesam/meceiot/features/lorawan/presentation/LoraWanAboutFragment.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Observer
+import coil3.load
 import com.example.android_meceiot.databinding.FragmentAboutLorawanBinding
+import edu.iesam.meceiot.features.lorawan.domain.LoraWanInfo
 
-class LoraWanAboutFragment: Fragment() {
+class LoraWanAboutFragment : Fragment() {
     private lateinit var loraWanFactory: LoraWanFactory
     private lateinit var loraWanViewModel: LoraWanViewModel
 
@@ -22,17 +24,54 @@ class LoraWanAboutFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentAboutLorawanBinding.inflate(inflater, container, false)
-        //setupView()
         return binding.root
     }
-
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         loraWanFactory = LoraWanFactory(requireContext())
         loraWanViewModel = loraWanFactory.provideGetInfoLoraWan()
-        //setupObserver()
+        setupObserver()
         loraWanViewModel.viewCreated()
+    }
+
+    private fun setupObserver() {
+        val loraWanObserver = Observer<LoraWanViewModel.UiState> { uiState ->
+            uiState.infoLoraWan?.let {
+                bindData(it)
+            }
+            uiState.errorApp?.let {
+                //print error
+            } ?: run {
+                // hide error
+            }
+            if (uiState.isLoading) {
+                //show loading
+            } else {
+                //hide loading
+            }
+        }
+        loraWanViewModel.uiState.observe(viewLifecycleOwner, loraWanObserver)
+    }
+
+    private fun bindData(loraWanInfo: List<LoraWanInfo>) {
+        val url = "https://n9.cl/nb8pg"
+
+        binding.titleInfo1.text
+        binding.imageInfo1.load(url)
+        binding.description4.text
+
+        binding.titleInfo2.text
+        binding.imageInfo2.load(url)
+        binding.description2.text
+
+        binding.titleInfo3.text
+        binding.imageInfo3.load(url)
+        binding.description3.text
+
+        binding.titleInfo4.text
+        binding.imageInfo4.load(url)
+        binding.description4.text
     }
 }

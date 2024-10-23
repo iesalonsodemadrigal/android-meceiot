44401a34e33cdcae34efd239768bad66fc59af45
derelop <dereklopezgarcia@gamil.com>
2024-10-23 16:59:57 +0200

    feat: navigation menu try 1

--- app/src/main/java/edu/iesam/meceiot/MainActivity.kt
+++ app/src/main/java/edu/iesam/meceiot/MainActivity.kt
@@ -1,21 +1,26 @@
 package edu.iesam.meceiot
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.android_meceiot.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setupView()
+    }
+
+    private fun setupView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        findViewById<BottomNavigationView>(R.id.main_menu).setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
         }
     }
 }
---
+++ app/src/main/java/edu/iesam/meceiot/features/presentation/FragmentC.kt
@@ -0,0 +1,26 @@
+package edu.iesam.meceiot.features.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.android_meceiot.R
+import com.google.android.material.appbar.MaterialToolbar
+
+class FragmentC : Fragment(R.layout.fragment_c) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        view?.findViewById<MaterialToolbar>(R.id.main_menu)?.apply {
+            setNavigationOnClickListener {
+                findNavController().navigateUp()
+            }
+        }
+        return view
+    }
+}

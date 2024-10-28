7e10923a9529ef101223d45ff9d2a95509cfd4b4
Darnell Jose Livent Gonzalez <djliventg@gmail.com>
2024-10-28 20:16:09 +0100

    Home Menu (#11)
    * feat: navigation menu try 1
    
    * feat: The function of the button navigation works good, the problem is the fragment_a.xml in the label <fragment>
    
    * chore: feature/8/Home_Menu Correcion: biding , tools
    
    ---------
    
    Co-authored-by: derelop <dereklopezgarcia@gamil.com>
    Co-authored-by: Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.navigation.safeargs.kotlin)
 }
 
 android {
@@ -33,6 +34,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -42,7 +47,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.navigation.fragment.v280)
+    implementation(libs.androidx.navigation.ui)
+
 }

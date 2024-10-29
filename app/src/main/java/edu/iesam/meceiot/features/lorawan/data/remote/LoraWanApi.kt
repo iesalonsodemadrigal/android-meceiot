14170c025984573b5f7b83ad8c571a6ef8a97b13
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-10-29 20:35:36 +0100

    feat: Added a map to the api model, because the domain should never depend on data

---
+++ app/src/main/java/edu/iesam/meceiot/features/lorawan/data/remote/LoraWanApi.kt
@@ -0,0 +1,8 @@
+package edu.iesam.meceiot.features.lorawan.data.remote
+
+data class LoraWanApiModel(
+    val id: String,
+    val title: String,
+    val image: String,
+    val description: String
+)

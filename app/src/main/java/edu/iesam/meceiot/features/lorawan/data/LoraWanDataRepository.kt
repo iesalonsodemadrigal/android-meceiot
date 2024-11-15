3860ca620ce7308fcabdca2bafd1c53af76685d5
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-11-15 17:47:26 +0100

    chore: Deleted the comments

--- app/src/main/java/edu/iesam/meceiot/features/lorawan/data/LoraWanDataRepository.kt
+++ app/src/main/java/edu/iesam/meceiot/features/lorawan/data/LoraWanDataRepository.kt
@@ -1,6 +1,6 @@
 package edu.iesam.meceiot.features.lorawan.data
 
-import edu.iesam.meceiot.core.domain.ErrorApp
+
 import edu.iesam.meceiot.features.lorawan.data.local.LoraWanXmlLocalDataSource
 import edu.iesam.meceiot.features.lorawan.data.remote.LoraWanApiRemoteDataSource
 import edu.iesam.meceiot.features.lorawan.domain.LoraWanInfo
@@ -17,15 +17,9 @@ class LoraWanDataRepository(
         val loraWanInfoFromLocal = loraWanXmlLocalDataSource.getLoraWanInfo()
 
         return if (loraWanInfoFromLocal.isEmpty()) {
-            val loraWanInfoFromRemote = loraWanApiRemoteDataSource.getInfoLoraWan()
-            return loraWanInfoFromRemote.fold(
-                onSuccess = { loraWanInfo ->
-                    loraWanXmlLocalDataSource.saveAll(loraWanInfo)
-                    Result.success(loraWanInfo)
-                },
-                onFailure = { error ->
-                    Result.failure(error as? ErrorApp ?: ErrorApp.DataErrorApp)
-                })
+            loraWanApiRemoteDataSource.getInfoLoraWan().onSuccess {
+                loraWanXmlLocalDataSource.saveAll(it)
+            }
         } else {
             Result.success(loraWanInfoFromLocal)
         }

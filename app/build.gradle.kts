e430278f5cedb6a67a410d9a3d13e2978fc19d2f
Miguel Alvarez Rubio <114093648+amrubio27@users.noreply.github.com>
2024-11-15 03:26:23 +0100

    Pantalla de login de usuario (#3)
    * start branch
    -mod. gitignore
    
    * feature Login
    -(WIP) Created classes and resources to start working on it
    
    * feature Login
    -(WIP) Working on the fragment
    
    * feature Login
    -(WIP) Working on the fragment
    
    * feature Login
    -(WIP) Working on the fragment
    
    * feature Login
    -(WIP) Working on the fragment
    
    * feature Login
    -(WIP) Working on the fragment
    
    * feature Login
    -(WIP) Working on the fragment
    
    * feature Login
    -(WIP) Working on the fragment
    
    * feature Login
    -Working login with debug toast
    
    * feature Login
    -Working login with debug toast
    
    * feature Login
    ...
    
    * feature Login
    testing? idk my first time testing
    
    * feature Login
    implement MockK Library for UnitTest
    
    * feature Login
    checking comments and making changes accordingly...
    
    * feature LoginTest:
    correcting files
    
    * feature LoginTest:
    refactoring...
    hardcoded chema credential to test...
    
    * feature LoginTest:
    delete credentials
    
    * feature LoginTest:
    change return type of apiService to new class LoginResponse, which is a boolean.
    
    * Merge remote-tracking branch 'origin/master' into feature/1/pantalla_login
    
    # Conflicts:
    # conflicts everywhere...
    
    * Merge remote-tracking branch 'origin/master' into feature/1/pantalla_login
    
    # Conflicts:
    # conflicts everywhere...
    
    * Merge remote-tracking branch 'origin/master' into feature/1/pantalla_login
    
    # Conflicts:
    # conflicts everywhere...
    
    * Merge remote-tracking branch 'origin/master' into feature/1/pantalla_login
    
    # Conflicts:
    # conflicts everywhere...
    
    * Merge remote-tracking branch 'origin/master' into feature/1/pantalla_login
    
    # Conflicts:
    # conflicts everywhere...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
     buildFeatures {
         viewBinding = true
     }
@@ -43,46 +42,38 @@ android {
 
 dependencies {
 
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-
-
+    //Lifecycle
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    //LiveData
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    //Navigation
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.legacy.support.v4)
-
-
+    //Gson
     implementation(libs.gson.serializer)
+    //Retrofit
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter)
     implementation(libs.okhttp.log.interceptor)
-
-
+    //Koin
     implementation(project.dependencies.platform(libs.koin.bom))
     implementation(libs.koin.android)
     implementation(libs.koin.annotations)
-
-    //KSP
     ksp(libs.koin.ksp)
-
-
-
-
-    testImplementation(libs.junit)
+    //MockK
     testImplementation(libs.mockk)
+    testImplementation(libs.mockk.android)
+    testImplementation(libs.mockk.agent)
+    //JUnit
+    testImplementation(libs.junit)
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.junit.jupiter.engine)
-
-
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-}
-ksp {
-    arg("KOIN_CONFIG_CHECK", "true")
 }

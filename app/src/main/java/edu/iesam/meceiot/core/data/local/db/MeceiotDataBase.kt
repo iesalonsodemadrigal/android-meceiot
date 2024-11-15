31a6c09e41b35d82ddb092cf25c741e7870b1e68
BORJJIM <borjajg02@gmail.com>
2024-11-15 18:45:21 +0100

    core : Add Room

--- app/src/main/java/edu/iesam/meceiot/core/data/local/db/MeceiotDataBase.kt
+++ app/src/main/java/edu/iesam/meceiot/core/data/local/db/MeceiotDataBase.kt
@@ -4,8 +4,13 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-
+//@Database( version = 1, exportSchema = false)
+@TypeConverters()
 abstract class MeceiotDataBase : RoomDatabase() {
+
+    //abstract fun loraWanDao(): LoraWanDao
+    //abstract fun developerDao(): DeveloperDao
+    //abstract fun testDao(): TestDao
 
 
 }

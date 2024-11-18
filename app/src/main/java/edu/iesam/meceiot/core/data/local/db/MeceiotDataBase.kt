ca77e49b0b57f311b2b937a53e2a54ce823deb0c
BORJJIM <borjajg02@gmail.com>
2024-11-18 20:22:52 +0100

    chore: Correction of the pr

--- app/src/main/java/edu/iesam/meceiot/core/data/local/db/MeceiotDataBase.kt
+++ app/src/main/java/edu/iesam/meceiot/core/data/local/db/MeceiotDataBase.kt
@@ -1,16 +1,10 @@
 package edu.iesam.meceiot.core.data.local.db
 
-import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-//@Database( version = 1, exportSchema = false)
+//@Database(entities = [], version = 1, exportSchema = false)
 @TypeConverters()
 abstract class MeceiotDataBase : RoomDatabase() {
-
-    //abstract fun loraWanDao(): LoraWanDao
-    //abstract fun developerDao(): DeveloperDao
-    //abstract fun testDao(): TestDao
-
 
 }

3253a4a9fff2b3618822d61fa353b1b9b0517045
Darnell Jose Livent Gonzalez <djliventg@gmail.com>
2024-11-18 02:42:51 +0100

    implementación ApiCall (#32)
    * core: primer commit , añadidos los ficheros pedidos , resolucion de rutas de paquete , control de librerias
    
    * Core: correcion de problemas rutas paquetes , implementacion de libreria

--- app/src/main/java/edu/iesam/meceiot/core/domain/ErrorApp.kt
+++ app/src/main/java/edu/iesam/meceiot/core/domain/ErrorApp.kt
@@ -1,8 +1,19 @@
 package edu.iesam.meceiot.core.domain
 
-sealed class ErrorApp {
-    object InternetErrorApp : ErrorApp()
-    object DatabaseErrorApp : ErrorApp()
-    object UnknowErrorApp : ErrorApp()
-    object ServerErrorApp : ErrorApp()
+sealed class ErrorApp : Throwable() {
+    object InternetErrorApp : ErrorApp() {
+        private fun readResolve(): Any = InternetErrorApp
+    }
+
+    object ServerErrorApp : ErrorApp() {
+        private fun readResolve(): Any = ServerErrorApp
+    }
+
+    object DataErrorApp : ErrorApp() {
+        private fun readResolve(): Any = DataErrorApp
+    }
+
+    object UnknownErrorApp : ErrorApp() {
+        private fun readResolve(): Any = UnknownErrorApp
+    }
 }

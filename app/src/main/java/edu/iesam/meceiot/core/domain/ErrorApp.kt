04c3cd2729f03cff285910addd34747dae0ab89e
DerekLopezGarcia <115727176+DerekLopezGarcia@users.noreply.github.com>
2024-11-19 17:08:51 +0100

    chore 24 Suggested changes

--- app/src/main/java/edu/iesam/meceiot/core/domain/ErrorApp.kt
+++ app/src/main/java/edu/iesam/meceiot/core/domain/ErrorApp.kt
@@ -1,8 +1,19 @@
 package edu.iesam.meceiot.core.domain
 
 sealed class ErrorApp : Throwable() {
-    object InternetErrorApp : ErrorApp()
-    object ServerErrorApp : ErrorApp()
-    object DataErrorApp : ErrorApp()
-    object UnknownErrorApp : ErrorApp()
+    object InternetErrorApp : ErrorApp() {
+        private fun readResolve(): Any = InternetErrorApp
+    }
+
+    object ServerErrorApp : ErrorApp() {
+        private fun readResolve(): Any = ServerErrorApp
+    }
+
+    object DataErrorApp : ErrorApp() {
+        private fun readResolve(): Any = DataErrorApp
+    }
+
+    object UnknownErrorApp : ErrorApp() {
+        private fun readResolve(): Any = UnknownErrorApp
+    }
 }

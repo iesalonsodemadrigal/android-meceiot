7d6ae63335aa1df96f22efa45682f50ea801df2a
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-12-01 20:37:50 +0100

    chore: delete cache TTL

--- app/src/main/java/edu/iesam/meceiot/features/lorawan/data/local/db/LoraWanEntity.kt
+++ app/src/main/java/edu/iesam/meceiot/features/lorawan/data/local/db/LoraWanEntity.kt
@@ -1,10 +1,8 @@
 package edu.iesam.meceiot.features.lorawan.data.local.db
 
 import androidx.room.ColumnInfo
-import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.Date
 
 const val LORAWAN_TABLE = "lorawan"
 const val LORAWAN_ID = "lorawan_id"
@@ -14,6 +12,5 @@ class LoraWanEntity(
     @PrimaryKey @ColumnInfo(name = "LORAWAN_ID") val id: String,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "image") val image: String,
-    @ColumnInfo(name = "description") val description: String,
-    @Embedded (prefix = "last_date") val date: Date
+    @ColumnInfo(name = "description") val description: String
 )

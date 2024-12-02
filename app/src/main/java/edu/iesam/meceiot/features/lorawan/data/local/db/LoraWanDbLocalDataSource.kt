8fb5feea49724b4b1f50f7b3f8e3e2824d0a32f8
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-12-02 20:22:40 +0100

    chore: cache ttl generic

--- app/src/main/java/edu/iesam/meceiot/features/lorawan/data/local/db/LoraWanDbLocalDataSource.kt
+++ app/src/main/java/edu/iesam/meceiot/features/lorawan/data/local/db/LoraWanDbLocalDataSource.kt
@@ -3,6 +3,7 @@ package edu.iesam.meceiot.features.lorawan.data.local.db
 import edu.iesam.meceiot.features.lorawan.domain.LoraWanInfo
 import org.koin.core.annotation.Single
 
+const val TIME_LORAWAN_TTL = 60000L
 
 @Single
 class LoraWanDbLocalDataSource(
@@ -10,7 +11,13 @@ class LoraWanDbLocalDataSource(
 ) {
 
     suspend fun getAll(): List<LoraWanInfo> {
+        val lastTime = System.currentTimeMillis()
         val loraWanInfoEntities = loraWanDao.getAll()
+
+        loraWanInfoEntities.filter{ entity ->
+            val timeDifferent = lastTime - entity.date.time
+            timeDifferent <= TIME_LORAWAN_TTL
+        }
         return loraWanInfoEntities.map { it.toDomain() }
     }
 

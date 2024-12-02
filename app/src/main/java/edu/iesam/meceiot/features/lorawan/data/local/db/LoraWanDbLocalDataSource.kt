90fb325e44320dc684b5806ccc03086d2f4ddf4f
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-12-02 02:21:01 +0100

    chore: add DataExpiredError

--- app/src/main/java/edu/iesam/meceiot/features/lorawan/data/local/db/LoraWanDbLocalDataSource.kt
+++ app/src/main/java/edu/iesam/meceiot/features/lorawan/data/local/db/LoraWanDbLocalDataSource.kt
@@ -1,6 +1,7 @@
 package edu.iesam.meceiot.features.lorawan.data.local.db
 
 import android.util.Log
+import edu.iesam.meceiot.core.domain.ErrorApp
 import edu.iesam.meceiot.features.lorawan.domain.LoraWanInfo
 import org.koin.core.annotation.Single
 
@@ -12,7 +13,7 @@ class LoraWanDbLocalDataSource(
     //,private val cacheCheck: CacheCheck<LoraWanEntity>
 ) {
 
-    suspend fun getAll(): List<LoraWanInfo> {
+    suspend fun getAll(): Result<List<LoraWanInfo>> {
         val currentTime = System.currentTimeMillis()
         val loraWanInfoEntities = loraWanDao.getAll()
 
@@ -21,11 +22,17 @@ class LoraWanDbLocalDataSource(
             timeDifference <= TIME_LORAWAN_TTL
         }
 
+        //simplemente es para saber que se guarda la hora bien y luego se convierte a Long
         loraWanInfoEntities.forEach { entity ->
             Log.d("@dev", "Esto funciona? Fecha de guardado en db: ${entity.date}")
         }
 
-        return validEntities.map { it.toDomain() }
+        //debemos lanzar un error cuando se tarda más del tiempo estipulado (ErrorApp??)
+        return if (validEntities.isEmpty()) {
+            Result.failure(ErrorApp.DataErrorApp) //Aquí deberíamos crear un DataExpiredError
+        } else {
+            Result.success(validEntities.map { it.toDomain() })
+        }
         //val loraWanCache = cacheCheck.execute(TIME_LORAWAN_TTL)
         //return loraWanCache.map { it.toDomain() }
     }

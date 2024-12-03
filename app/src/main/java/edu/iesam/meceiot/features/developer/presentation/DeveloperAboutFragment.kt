0032330b4248ea7f33df3a01863143120f89f886
BORJJIM <borjajg02@gmail.com>
2024-12-03 20:34:14 +0100

    chore: Correction of the conflicts

--- app/src/main/java/edu/iesam/meceiot/features/developer/presentation/DeveloperAboutFragment.kt
+++ app/src/main/java/edu/iesam/meceiot/features/developer/presentation/DeveloperAboutFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import edu.iesam.meceiot.core.presentation.AppIntent
@@ -14,6 +13,7 @@ import edu.iesam.meceiot.features.developer.domain.models.DeveloperInfo
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DeveloperAboutFragment : BottomSheetDialogFragment() {
+
 
     private val developerAboutViewModel: DeveloperAboutViewModel by viewModel()
     private lateinit var appIntent: AppIntent
@@ -29,6 +29,7 @@ class DeveloperAboutFragment : BottomSheetDialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDeveloperListBinding.inflate(inflater, container, false)
+        appIntent = AppIntent(requireContext())
         setupView()
         return binding.root
     }
@@ -36,21 +37,16 @@ class DeveloperAboutFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        appIntent = AppIntent(requireContext())
-
-        developerFactory = DeveloperFactory(requireContext())
-        developerAboutViewModel = developerFactory.provideGetDevelopers()
         setupObserver()
         developerAboutViewModel.viewDevelopers()
     }
 
     private fun setupObserver() {
-        val developerObserver = Observer<DeveloperAboutViewModel.UiState> { uiState ->
+        developerAboutViewModel.uiState.observe(viewLifecycleOwner) { uiState ->
             uiState.infoDeveloper?.let {
                 bindData(it)
             }
         }
-        developerAboutViewModel.uiState.observe(viewLifecycleOwner, developerObserver)
     }
 
     private fun setupView() {
@@ -63,7 +59,7 @@ class DeveloperAboutFragment : BottomSheetDialogFragment() {
     }
 
     private fun bindData(developers: List<DeveloperInfo>) {
-        developerAdapter.submitList(developers.sortedBy { it.id.toInt() })
+        developerAdapter.submitList(developers.sortedBy { it.id.toInt() }) // Ordena por ID
     }
 
     override fun onDestroyView() {

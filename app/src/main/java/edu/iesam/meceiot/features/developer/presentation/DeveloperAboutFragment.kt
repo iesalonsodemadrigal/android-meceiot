c83f8ccaeb2c77f8e4a0da19a5e88e260a256e88
BORJJIM <borjajg02@gmail.com>
2024-12-03 19:54:19 +0100

    feat: Add appintent-core into DeveloperAboutFragment.kt

--- app/src/main/java/edu/iesam/meceiot/features/developer/presentation/DeveloperAboutFragment.kt
+++ app/src/main/java/edu/iesam/meceiot/features/developer/presentation/DeveloperAboutFragment.kt
@@ -1,8 +1,6 @@
 package edu.iesam.meceiot.features.developer.presentation
 
 import DeveloperAdapter
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +8,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import edu.iesam.meceiot.core.presentation.AppIntent
 import edu.iesam.meceiot.databinding.FragmentDeveloperListBinding
 import edu.iesam.meceiot.features.developer.domain.models.DeveloperInfo
 
@@ -17,12 +16,13 @@ class DeveloperAboutFragment : BottomSheetDialogFragment() {
 
     private lateinit var developerFactory: DeveloperFactory
     private lateinit var developerAboutViewModel: DeveloperAboutViewModel
+    private lateinit var appIntent: AppIntent
 
     private var _binding: FragmentDeveloperListBinding? = null
     private val binding get() = _binding!!
 
     private val developerAdapter = DeveloperAdapter { url ->
-        openUrl(url)
+        appIntent.openUrl(url)
     }
 
     override fun onCreateView(
@@ -36,12 +36,12 @@ class DeveloperAboutFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        appIntent = AppIntent(requireContext())
+
         developerFactory = DeveloperFactory(requireContext())
         developerAboutViewModel = developerFactory.provideGetDevelopers()
         setupObserver()
         developerAboutViewModel.viewDevelopers()
-
-
     }
 
     private fun setupObserver() {
@@ -63,13 +63,7 @@ class DeveloperAboutFragment : BottomSheetDialogFragment() {
     }
 
     private fun bindData(developers: List<DeveloperInfo>) {
-
         developerAdapter.submitList(developers.sortedBy { it.id.toInt() })
-    }
-
-    private fun openUrl(url: String) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        startActivity(intent)
     }
 
     override fun onDestroyView() {

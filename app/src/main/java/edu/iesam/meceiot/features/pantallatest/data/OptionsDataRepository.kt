10bbb3459b2766881cdf65a8cd743dbdbf8a933c
DARNLIV <djliventg@gmail.com>
2024-12-05 16:57:08 +0100

    feature : borrado de clases no necesarias

--- app/src/main/java/edu/iesam/meceiot/features/pantallatest/data/OptionsDataRepository.kt
+++ app/src/main/java/edu/iesam/meceiot/features/pantallatest/data/OptionsDataRepository.kt
@@ -2,14 +2,14 @@ package edu.iesam.meceiot.features.pantallatest.data
 
 import edu.iesam.meceiot.features.pantallatest.domain.Question
 
-class OptionsDataRepository {
+class OptionsDataRepository : OptionsDataSource {
     private val questions = mutableListOf<Question>()
 
-    fun getSelectedOptions(): List<Question> {
+    override fun getSelectedOptions(): List<Question> {
         return questions
     }
 
-    fun updateSelectedOption(question: Question) {
+    override fun updateSelectedOption(question: Question) {
         val index = questions.indexOfFirst { it.id == question.id }
         if (index != -1) {
             questions[index] = question

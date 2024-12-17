4d0324fd6295a58c2cc6cce39427731d752782ef
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-12-17 15:45:47 +0100

    chore: other form to create the skeleton layout

--- app/src/main/java/edu/iesam/meceiot/features/lorawan/presentation/LoraWanViewModel.kt
+++ app/src/main/java/edu/iesam/meceiot/features/lorawan/presentation/LoraWanViewModel.kt
@@ -8,6 +8,7 @@ import edu.iesam.meceiot.core.domain.ErrorApp
 import edu.iesam.meceiot.features.lorawan.domain.GetInfoLoraWanUseCase
 import edu.iesam.meceiot.features.lorawan.domain.LoraWanInfo
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
 

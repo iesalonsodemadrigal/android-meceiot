19650406853ceee4786129e6c73f4687bba13173
Borja <115727186+BorjaJG@users.noreply.github.com>
2025-01-10 17:16:34 +0100

    feat: cracion de feature

--- app/src/main/java/edu/iesam/meceiot/features/presentation/Settings.kt
+++ app/src/main/java/edu/iesam/meceiot/features/presentation/Settings.kt
@@ -10,13 +10,13 @@ import androidx.navigation.fragment.findNavController
 import edu.iesam.meceiot.R
 
 
-class FragmentB : Fragment(R.layout.fragment_b) {
+class Settings : Fragment(R.layout.fragment_settings) {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
-        val view = inflater.inflate(R.layout.fragment_b, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
         val navigateButton: Button = view.findViewById(R.id.button_navigate)
         navigateButton.setOnClickListener {

61bf78690cb93bf9ffb7e277c12b787fcebff096
Borja <115727186+BorjaJG@users.noreply.github.com>
2025-01-10 17:49:11 +0100

    feat: Created 'Settings' fragment and added 'Developers' and 'Digital Resources

--- app/src/main/java/edu/iesam/meceiot/features/presentation/Settings.kt
+++ app/src/main/java/edu/iesam/meceiot/features/presentation/Settings.kt
@@ -1,28 +1,30 @@
 package edu.iesam.meceiot.features.presentation
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import edu.iesam.meceiot.R
 
-
 class Settings : Fragment(R.layout.fragment_settings) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val view = inflater.inflate(R.layout.fragment_settings, container, false)
-
-        val navigateButton: Button = view.findViewById(R.id.button_navigate)
-        navigateButton.setOnClickListener {
-            val action = FragmentBDirections.actionFragmentBToDeveloperAboutFragment()
-            findNavController().navigate(action)
+        val navigateButtonDeveloper: Button = view.findViewById(R.id.button_navigate_developer)
+        navigateButtonDeveloper.setOnClickListener {
+            navigateTo(SettingsDirections.actionSettingsToDeveloperAboutFragment())
         }
-        return view
+
+        val navigateButtonExternalResources: Button = view.findViewById(R.id.button_navigate_externalResource)
+        navigateButtonExternalResources.setOnClickListener {
+            navigateTo(SettingsDirections.actionSettingsToExternalResourcesFragment())
+        }
+    }
+
+    private fun navigateTo(action: NavDirections) {
+        findNavController().navigate(action)
     }
 }

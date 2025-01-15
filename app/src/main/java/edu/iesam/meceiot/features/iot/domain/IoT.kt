3e72f0a96474c4ae07c7eb265b0df60311c0afe1
Darnell Jose Livent Gonzalez <djliventg@gmail.com>
2025-01-15 16:49:26 +0100

    feature : implementacion de las correciones chema (No se suben mocks.                                                    Inglés en namings de ramas.                                                    Datos estáticos no es necesario usar RecyclerView.                                                    Strings sin internacionalización.                                                    No hace falta caso de uso, ni repositorio, ni capa de datos.                                                    Errores?? Loading??)

--- app/src/main/java/edu/iesam/meceiot/features/iot/domain/IoT.kt
+++
@@ -1,8 +0,0 @@
-package edu.iesam.meceiot.features.iot.domain
-
-data class IoT(
-    val id: String,
-    val title: String,
-    val image: String,
-    val description: String
-)
--- app/src/main/java/edu/iesam/meceiot/features/iot/domain/IotRepository.kt
+++
@@ -1,6 +0,0 @@
-package edu.iesam.meceiot.features.iot.domain
-
-interface IotRepository {
-
-    fun getIoTList(): List<IoT>
-}

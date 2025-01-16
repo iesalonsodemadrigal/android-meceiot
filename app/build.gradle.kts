b0541ca6b76d73f7a2151b2bf4649c9f31047548
Borja <borjajg02@gmail.com>
2025-01-16 10:46:45 +0100

    feat: Modyfi MaterialToolbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,6 @@ dependencies {
     implementation(project.dependencies.platform(libs.koin.bom))
     implementation(libs.koin.android)
     implementation(libs.koin.annotations)
-    implementation(libs.androidx.cardview)
     ksp(libs.koin.ksp)
     //MockK
     testImplementation(libs.mockk)

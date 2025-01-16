c7ba6d1af2ba0ff6ef6947a74904d19828e916e8
Djliv <djliventg@gmail.com>
2025-01-16 10:42:15 +0100

    Feature:correciones borja (sustituir view por binding)

--- app/src/main/java/edu/iesam/meceiot/features/iot/presentation/IoTAboutFragment.kt
+++ app/src/main/java/edu/iesam/meceiot/features/iot/presentation/IoTAboutFragment.kt
@@ -4,31 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import edu.iesam.meceiot.R
+import edu.iesam.meceiot.databinding.FragmentIotBinding
+import edu.iesam.meceiot.databinding.ItemIotBinding
 
 class IoTFragment : Fragment() {
+
+    private var _binding: FragmentIotBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_iot, container, false)
+    ): View {
+        _binding = FragmentIotBinding.inflate(inflater, container, false)
 
+        setItemText(ItemIotBinding.bind(binding.item1.root), R.string.title_item_1, R.string.description_item_1)
+        setItemText(ItemIotBinding.bind(binding.item2.root), R.string.title_item_2, R.string.description_item_2)
+        setItemText(ItemIotBinding.bind(binding.item3.root), R.string.title_item_3, R.string.description_item_3)
+        setItemText(ItemIotBinding.bind(binding.item4.root), R.string.title_item_4, R.string.description_item_4)
+        setItemText(ItemIotBinding.bind(binding.item5.root), R.string.title_item_5, R.string.description_item_5)
 
-        setItemText(view, R.id.item_1, R.string.title_item_1, R.string.description_item_1)
-        setItemText(view, R.id.item_2, R.string.title_item_2, R.string.description_item_2)
-        setItemText(view, R.id.item_3, R.string.title_item_3, R.string.description_item_3)
-        setItemText(view, R.id.item_4, R.string.title_item_4, R.string.description_item_4)
-        setItemText(view, R.id.item_5, R.string.title_item_5, R.string.description_item_5)
-
-        return view
+        return binding.root
     }
 
-    private fun setItemText(view: View, itemId: Int, titleId: Int, descriptionId: Int) {
-        val itemView = view.findViewById<View>(itemId)
-        itemView.findViewById<TextView>(R.id.itemTitle).setText(titleId)
-        itemView.findViewById<TextView>(R.id.itemDescription).setText(descriptionId)
+    private fun setItemText(itemBinding: ItemIotBinding, titleId: Int, descriptionId: Int) {
+        itemBinding.itemTitle.setText(titleId)
+        itemBinding.itemDescription.setText(descriptionId)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }

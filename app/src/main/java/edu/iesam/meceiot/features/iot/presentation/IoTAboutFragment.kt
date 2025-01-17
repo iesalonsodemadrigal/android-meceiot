1d59e92f680eb9c0b9e01cebb76450d082b31424
Darnell Jose Livent Gonzalez <djliventg@gmail.com>
2025-01-17 17:57:13 +0100

    feature: correciones y recomendaciones de chema

--- app/src/main/java/edu/iesam/meceiot/features/iot/presentation/IoTAboutFragment.kt
+++ app/src/main/java/edu/iesam/meceiot/features/iot/presentation/IoTAboutFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
 import edu.iesam.meceiot.R
 import edu.iesam.meceiot.databinding.FragmentIotBinding
 import edu.iesam.meceiot.databinding.ItemIotBinding
@@ -20,18 +21,42 @@ class IoTFragment : Fragment() {
     ): View {
         _binding = FragmentIotBinding.inflate(inflater, container, false)
 
-        setItemText(ItemIotBinding.bind(binding.item1.root), R.string.title_item_1, R.string.description_item_1)
-        setItemText(ItemIotBinding.bind(binding.item2.root), R.string.title_item_2, R.string.description_item_2)
-        setItemText(ItemIotBinding.bind(binding.item3.root), R.string.title_item_3, R.string.description_item_3)
-        setItemText(ItemIotBinding.bind(binding.item4.root), R.string.title_item_4, R.string.description_item_4)
-        setItemText(ItemIotBinding.bind(binding.item5.root), R.string.title_item_5, R.string.description_item_5)
+
+        val toolbar = binding.root.findViewById<MaterialToolbar>(R.id.view_toolbar_title)
+        toolbar.title = getString(R.string.iot_title)
+
+        setItemText(
+            ItemIotBinding.bind(binding.viewItemIot.root),
+            R.string.title_item_1,
+            R.string.description_item_1
+        )
+        setItemText(
+            ItemIotBinding.bind(binding.viewItemPlanesEducativos.root),
+            R.string.title_item_2,
+            R.string.description_item_2
+        )
+        setItemText(
+            ItemIotBinding.bind(binding.viewItemCalendario.root),
+            R.string.title_item_3,
+            R.string.description_item_3
+        )
+        setItemText(
+            ItemIotBinding.bind(binding.viewItemProgramas.root),
+            R.string.title_item_4,
+            R.string.description_item_4
+        )
+        setItemText(
+            ItemIotBinding.bind(binding.viewItemMeceiot.root),
+            R.string.title_item_5,
+            R.string.description_item_5
+        )
 
         return binding.root
     }
 
     private fun setItemText(itemBinding: ItemIotBinding, titleId: Int, descriptionId: Int) {
-        itemBinding.itemTitle.setText(titleId)
-        itemBinding.itemDescription.setText(descriptionId)
+        itemBinding.viewItemTitle.setText(titleId)
+        itemBinding.viewItemDescription.setText(descriptionId)
     }
 
     override fun onDestroyView() {

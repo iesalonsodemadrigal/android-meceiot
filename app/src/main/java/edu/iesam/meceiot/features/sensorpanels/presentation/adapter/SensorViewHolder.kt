6689813c8dedfce9c8d88c326abc75ec605a048d
Miguel Alvarez Rubio <amiguelrubio27@gmail.com>
2025-01-17 17:53:36 +0100

    feature/66: working on improvements

--- app/src/main/java/edu/iesam/meceiot/features/sensorpanels/presentation/adapter/SensorViewHolder.kt
+++ app/src/main/java/edu/iesam/meceiot/features/sensorpanels/presentation/adapter/SensorViewHolder.kt
@@ -11,6 +11,16 @@ class SensorViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
     fun bind(result: Sensor, onClickListener: (String) -> Unit) {
         itemSensor.apply {
             sensorName.text = result.name
+            /*when(sensorName){
+                "Temperature" -> sensorIcon.setImageResource(R.drawable.ic_temperature)
+                "Humidity" -> sensorIcon.setImageResource(R.drawable.ic_humidity)
+                "Light" -> sensorIcon.setImageResource(R.drawable.ic_light)
+                "Motion" -> sensorIcon.setImageResource(R.drawable.ic_motion)
+                "Sound" -> sensorIcon.setImageResource(R.drawable.ic_sound)
+                "Pressure" -> sensorIcon.setImageResource(R.drawable.ic_pressure)
+                "Smoke" -> sensorIcon.setImageResource(R.drawable.ic_smoke)
+                "CO2" -> sensorIcon.setImageResource(R.drawable.ic_co2)
+            }*/
             view.setOnClickListener {
                 onClickListener(result.id.toString())
             }

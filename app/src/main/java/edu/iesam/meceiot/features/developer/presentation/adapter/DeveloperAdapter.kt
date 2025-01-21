22c060b025dd8ffab1d85f3c1e6ed1679439c148
Borja <115727186+BorjaJG@users.noreply.github.com>
2025-01-21 17:01:21 +0100

    chore : delete dialog unnecessary

--- app/src/main/java/edu/iesam/meceiot/features/developer/presentation/adapter/DeveloperAdapter.kt
+++ app/src/main/java/edu/iesam/meceiot/features/developer/presentation/adapter/DeveloperAdapter.kt
@@ -1,12 +1,9 @@
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.constraintlayout.motion.widget.MotionScene.Transition.TransitionOnClick
 import androidx.recyclerview.widget.ListAdapter
 import edu.iesam.meceiot.R
 import edu.iesam.meceiot.features.developer.domain.models.DeveloperInfo
 import edu.iesam.meceiot.features.developer.presentation.adapter.DeveloperDiffUtil
-import retrofit2.http.Url
 
 class DeveloperAdapter : ListAdapter<DeveloperInfo, DeveloperViewHolder>(DeveloperDiffUtil()) {
 
@@ -21,13 +18,9 @@ class DeveloperAdapter : ListAdapter<DeveloperInfo, DeveloperViewHolder>(Develop
             .inflate(R.layout.item_developer, parent, false)
         return DeveloperViewHolder(view)
     }
-
     override fun onBindViewHolder(holder: DeveloperViewHolder, position: Int) {
-        // Llamamos a bind para pasar la información del desarrollador y el click event
         holder.bind(getItem(position)) { url ->
-            // Aquí el código que se ejecutará cuando se haga clic en el item
-            Log.d("DeveloperAdapter", "URL clicked: $url")
-            onClick(url)  // Esto llama al onClick que se pasa como parámetro
+            onClick(url)
         }
     }
 

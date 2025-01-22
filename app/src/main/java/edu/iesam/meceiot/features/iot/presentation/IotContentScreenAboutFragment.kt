fc026e06b97880a6dc3703672167e729b3d3dcf5
Darnell Jose Livent Gonzalez <djliventg@gmail.com>
2025-01-22 16:13:09 +0100

    feat: frist commit

---
+++ app/src/main/java/edu/iesam/meceiot/features/iot/presentation/IotContentScreenAboutFragment.kt
@@ -0,0 +1,7 @@
+package edu.iesam.meceiot.features.iot.presentation
+
+import androidx.fragment.app.Fragment
+import edu.iesam.meceiot.R
+
+class IotContentScreenAboutFragment : Fragment(R.layout.fragment_iot_content) {
+}

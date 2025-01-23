bd26ecfd8c272dddf65ac47d1bc112e3b673aa62
Darnell Jose Livent Gonzalez <djliventg@gmail.com>
2025-01-23 19:16:40 +0100

    feat: 23/01 fix presentation

--- app/src/main/java/edu/iesam/meceiot/features/iot/presentation/IoTAboutFragment.kt
+++ app/src/main/java/edu/iesam/meceiot/features/iot/presentation/IoTAboutFragment.kt
@@ -7,64 +7,48 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import edu.iesam.meceiot.R
 import edu.iesam.meceiot.databinding.FragmentIotBinding
-import edu.iesam.meceiot.databinding.ItemIotBinding
 
 class IoTAboutFragment : Fragment(R.layout.fragment_iot) {
     private var _binding: FragmentIotBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentIotBinding.inflate(inflater, container, false)
-        bindData()
-
-        setItemText(
-            ItemIotBinding.bind(binding.viewItemMeceiot.root),
-            R.string.title_item_meceiot,
-            R.string.description_item_meceiot
-        )
-        setItemText(
-            ItemIotBinding.bind(binding.viewItemAlonso.root),
-            R.string.title_item_alonso,
-            R.string.description_item_iot_alonso
-        )
-        setItemText(
-            ItemIotBinding.bind(binding.viewItemInternet.root),
-            R.string.title_item_internet,
-            R.string.description_item_internet
-        )
-        setItemText(
-            ItemIotBinding.bind(binding.viewItemEnergySaving.root),
-            R.string.title_item_energy_saving,
-            R.string.description_item_energy_saving
-        )
-
-        setItemImage(
-            ItemIotBinding.bind(binding.viewItemMeceiot.root),
-            R.drawable.ic_logo_meceiot
-        )
-        setItemImage(
-            ItemIotBinding.bind(binding.viewItemAlonso.root),
-            R.drawable.ic_logo_iesam
-        )
-
+        setupView()
         return binding.root
     }
 
-    private fun setItemText(itemBinding: ItemIotBinding, titleId: Int, descriptionId: Int) {
-        itemBinding.viewItemTitle.setText(titleId)
-        itemBinding.viewItemDescription.setText(descriptionId)
-    }
-
-    private fun setItemImage(itemBinding: ItemIotBinding, imageResId: Int) {
-        itemBinding.viewItemImage.setImageResource(imageResId)
-    }
-
-    private fun bindData() {
+    private fun setupView() {
         binding.apply {
             viewToolbar.mainToolbar.title = getString(R.string.iot_title)
+
+            viewItemMeceiot.apply {
+                viewItemTitle.setText(R.string.title_item_meceiot)
+                viewItemDescription.setText(R.string.description_item_meceiot)
+                viewItemImage.setImageResource(R.drawable.ic_logo_meceiot)
+            }
+
+            viewItemAlonso.apply {
+                viewItemTitle.setText(R.string.title_item_alonso)
+                viewItemDescription.setText(R.string.description_item_iot_alonso)
+                viewItemImage.setImageResource(R.drawable.ic_logo_iesam)
+            }
+
+            viewItemInternet.apply {
+                viewItemTitle.setText(R.string.title_item_internet)
+                viewItemDescription.setText(R.string.description_item_internet)
+
+            }
+
+            viewItemEnergySaving.apply {
+                viewItemTitle.setText(R.string.title_item_energy_saving)
+                viewItemDescription.setText(R.string.description_item_energy_saving)
+
+            }
         }
     }
 
@@ -73,3 +57,5 @@ class IoTAboutFragment : Fragment(R.layout.fragment_iot) {
         _binding = null
     }
 }
+
+

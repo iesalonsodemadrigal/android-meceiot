287545f2e51bbc933c4187e11cda0647318a5093
Borja Jiménez Garcinuño <115727186+BorjaJG@users.noreply.github.com>
2025-01-24 07:39:25 +0100

    Settings screen design (#70)
    * feat: cracion de feature
    
    * feat: Created 'Settings' fragment and added 'Developers' and 'Digital Resources
    
    * feat: designing ddi settings
    
    * feat: Add version of App
    
    * feat:Completed the interface part
    
    * feat: Add share
    
    * feat: Correction of  email app
    
    * feat: ordenados strings.xml
    
    * feat: elimindos strings de los fragmentos 'b', 'c'
    
    * feat: Add version of App
    
    * feat: Add Styles
    
    * feat: Add item Setting
    
    * feat: Add Styles
    
    * feat: Modyfi MaterialToolbar
    
    * feat: Delete unused code
    
    * feat: Add item Setting
    
    * feat: correction strings.xml
    
    * feat: Added the lack of the gmail app
    
    * feat: Improved the code
    
    * feat : correction of name of ic_setting

--- app/src/main/java/edu/iesam/meceiot/core/presentation/AppIntent.kt
+++ app/src/main/java/edu/iesam/meceiot/core/presentation/AppIntent.kt
@@ -3,10 +3,13 @@ package edu.iesam.meceiot.core.presentation
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
+import edu.iesam.meceiot.R
 import org.koin.core.annotation.Single
 
 @Single
 class AppIntent(private val context: Context) {
+
     fun openUrl(url: String) {
         context.startActivity(
             Intent(
@@ -16,5 +19,39 @@ class AppIntent(private val context: Context) {
         )
     }
 
+    fun shareApp(shareText: String) {
+        Toast.makeText(context, context.getString(R.string.text_sharing_message), Toast.LENGTH_SHORT).show()
 
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, shareText)
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+
+        context.startActivity(Intent.createChooser(intent, context.getString(R.string.text_share_title)))
+    }
+
+    fun openEmail(recipient: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:$recipient")
+        }
+
+        try {
+            val emailApp = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:$recipient"))
+            emailApp.setPackage("com.google.android.gm") // Gmail
+            context.startActivity(emailApp)
+        } catch (e: Exception) {
+            try {
+                val hotmailApp = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:$recipient"))
+                hotmailApp.setPackage("com.microsoft.office.outlook") // Hotmail/Outlook
+                context.startActivity(hotmailApp)
+            } catch (e: Exception) {
+                try {
+                    context.startActivity(Intent.createChooser(intent, "Elige una app para enviar el correo"))
+                } catch (e: Exception) {
+                    println("No se encontró una app para enviar correos.")
+                }
+            }
+        }
+    }
 }

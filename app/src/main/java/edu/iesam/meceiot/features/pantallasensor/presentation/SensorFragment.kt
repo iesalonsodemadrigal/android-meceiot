d659f00c9a0ad86dbd6c2b4b1f6185fd8c4f9c06
Chema <josem.perram@educa.jcyl.es>
2025-01-31 09:06:10 +0100

    [chorus] solucionado algunos problemas

--- app/src/main/java/edu/iesam/meceiot/features/pantallasensor/presentation/SensorFragment.kt
+++ app/src/main/java/edu/iesam/meceiot/features/pantallasensor/presentation/SensorFragment.kt
@@ -1,19 +1,13 @@
 package edu.iesam.meceiot.features.pantallasensor.presentation
 
-import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.onNavDestinationSelected
 import com.faltenreich.skeletonlayout.Skeleton
 import com.google.android.material.appbar.MaterialToolbar
 import com.patrykandpatrick.vico.core.cartesian.Zoom
@@ -24,11 +18,12 @@ import com.patrykandpatrick.vico.core.cartesian.data.CartesianValueFormatter
 import com.patrykandpatrick.vico.core.cartesian.data.lineSeries
 import com.patrykandpatrick.vico.views.cartesian.CartesianChartView
 import com.patrykandpatrick.vico.views.cartesian.ZoomHandler
+import edu.iesam.meceiot.R
+import edu.iesam.meceiot.core.domain.ErrorApp
 import edu.iesam.meceiot.core.presentation.hide
 import edu.iesam.meceiot.core.presentation.views.ErrorAppFactory
 import edu.iesam.meceiot.databinding.FragmentSensorBinding
 import edu.iesam.meceiot.features.pantallasensor.domain.Sensor
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.util.Calendar
@@ -52,17 +47,31 @@ class SensorFragment : Fragment() {
         _binding = FragmentSensorBinding.inflate(inflater, container, false)
         setupView()
         cartesianChartView.modelProducer = modelProducer
-        setHasOptionsMenu(true)
         return binding.root
     }
 
     private fun setupView() {
-        val toolbar: MaterialToolbar = binding.toolbar.viewToolbarDetail
-        (activity as AppCompatActivity).setSupportActionBar(toolbar)
-        val actionBar: ActionBar? = (activity as AppCompatActivity).supportActionBar
-        actionBar?.setDisplayHomeAsUpEnabled(true)
-        actionBar?.setHomeButtonEnabled(true)
+        setupToolbar()
+
         cartesianChartView = binding.chart
+    }
+
+    private fun setupToolbar() {
+        val toolbar: MaterialToolbar = binding.toolbar.viewToolbarDetail
+        toolbar.inflateMenu(R.menu.options_graph_menu)
+        toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.fragment_options_graph -> {
+                    //findNavController().navigate(R.id.action_sensorFragment_to_optionsGraphFragment)
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -74,73 +83,56 @@ class SensorFragment : Fragment() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(edu.iesam.meceiot.R.menu.options_graph_menu, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.home -> {
-                findNavController().navigateUp()
-                true
-            }
-
-            edu.iesam.meceiot.R.id.fragment_options_graph -> {
-                true
-            }
-
-            else -> {
-                val navController = findNavController()
-                item.onNavDestinationSelected(navController)
-            }
-        }
-    }
-
-
     private fun setupObserver() {
         val sensorObserver = Observer<SensorViewModel.UiState> { uiState ->
-
-            uiState.sensors?.let { sensor ->
-                bindData(sensor)
-            }
-            uiState.errorApp?.let {
-                val errorAppUI = ErrorAppFactory(requireContext()).build(it) {
-                    getArgs()?.let { sensorId -> sensorViewModel.fetchSensorData(sensorId) }
-                }
-                binding.errorAppView.render(errorAppUI)
-            } ?: run {
-                binding.errorAppView.hide()
-            }
-            if (uiState.loading) {
-                skeleton.showSkeleton()
-            } else {
-                skeleton.showOriginal()
-            }
+            checkLoading(uiState.loading)
+            bindError(uiState.errorApp)
+            bindData(uiState.sensors)
         }
         sensorViewModel.uiState.observe(viewLifecycleOwner, sensorObserver)
     }
 
-    private fun getArgs(): Int? {
+    private fun checkLoading(isLoading: Boolean) {
+        if (isLoading) {
+            skeleton.showSkeleton()
+        } else {
+            skeleton.showOriginal()
+        }
+    }
+
+    private fun bindError(errorApp: ErrorApp?) {
+        errorApp?.let {
+            val errorAppUI = ErrorAppFactory(requireContext()).build(it) {
+                sensorViewModel.fetchSensorData(getArgs())
+            }
+            binding.errorAppView.render(errorAppUI)
+        } ?: run {
+            binding.errorAppView.hide()
+        }
+    }
+
+    private fun bindData(sensor: Sensor?) {
+        sensor?.let {
+            binding.apply {
+                initializeChart(it)
+                sensorName.text = it.name
+                toolbar.viewToolbarDetail.title = it.panelName
+                maxValue.text = it.maxValue
+                minValue.text = it.minValue
+                avgValue.text = it.avgValue
+                modeValue.text = it.modeValue
+            }
+        }
+    }
+
+    private fun getArgs(): Int {
         //Implement the logic to get the sensor id from the arguments
         return 1
     }
 
-    private fun bindData(sensor: Sensor) {
-        binding.apply {
-            initializeChart(sensor)
-            sensorName.text = sensor.name
-            toolbar.viewToolbarDetail.title = sensor.panelName
-            maxValue.text = sensor.maxValue
-            minValue.text = sensor.minValue
-            avgValue.text = sensor.avgValue
-            modeValue.text = sensor.modeValue
-        }
-    }
-
     private fun initializeChart(sensor: Sensor) {
         val chart = cartesianChartView.chart!!
-        GlobalScope.launch {
+        lifecycleScope.launch {
             modelProducer.runTransaction {
                 lineSeries {
                     series(sensor.xValues, sensor.yValues)

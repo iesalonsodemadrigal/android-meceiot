b836882e7fe71fea14727c980fb0e5d8017c62a6
Borja <borjajg02@gmail.com>
2025-02-02 22:38:03 +0100

    feat: correction Firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.navigation.safeargs.kotlin)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.google.services)
 }
 
 android {

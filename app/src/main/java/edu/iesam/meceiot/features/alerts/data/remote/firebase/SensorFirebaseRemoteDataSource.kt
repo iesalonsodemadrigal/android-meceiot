45c75660a358f0a1150275e029cde0a2bd0d6243
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2025-02-04 17:03:35 +0100

    chore: refactoring

--- app/src/main/java/edu/iesam/meceiot/features/alerts/data/remote/firebase/SensorFirebaseRemoteDataSource.kt
+++ app/src/main/java/edu/iesam/meceiot/features/alerts/data/remote/firebase/SensorFirebaseRemoteDataSource.kt
@@ -1,6 +1,5 @@
 package edu.iesam.meceiot.features.alerts.data.remote.firebase
 
-import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import edu.iesam.meceiot.features.alerts.domain.Sensor
 import kotlinx.coroutines.tasks.await
@@ -10,30 +9,17 @@ import org.koin.core.annotation.Single
 class SensorFirebaseRemoteDataSource(private val firestore: FirebaseFirestore) {
 
     suspend fun getSensors(): Result<List<Sensor>> {
-        // Obtener todos los documentos de la colección "alerts"
-        val querySnapshot = firestore.collection("alerts")
+        val alertsSensor = firestore.collection("alerts")
             .get()
             .await()
 
-        // Lista mutable para almacenar los sensores
         val sensorsList = mutableListOf<Sensor>()
 
-        // Iterar sobre todos los documentos de paneles
-        for (documentSnapshot in querySnapshot.documents) {
-            // Deserialización del documento
-            val panel = documentSnapshot.toObject(PanelAlertFirebaseModel::class.java)
-                ?: continue  // Si no se puede deserializar, continuar con el siguiente documento
+        for (document in alertsSensor.documents) {
+            val panels = document.toObject(PanelAlertFirebaseModel::class.java)
 
-            // Agregar los sensores de este panel a la lista
-            sensorsList.addAll(panel.sensors.values.map { it.toDomain() })
+            panels?.sensors?.values?.map { it.toDomain() }?.let { sensorsList.addAll(it) }
         }
-
-        // Verificar si se encontraron sensores
-        if (sensorsList.isEmpty()) {
-            return Result.failure(IllegalStateException("No se encontraron sensores en los paneles"))
-        }
-
-        Log.d("@dev", "Lista de sensores obtenida: $sensorsList")
         return Result.success(sensorsList)
     }
 }

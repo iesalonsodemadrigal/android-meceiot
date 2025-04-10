b654b623db0d0e231b79d751c38f7385d5cc5e6b
Chema <josem.perram@educa.jcyl.es>
2025-04-10 19:52:41 +0200

    [chorus] estilos y animaciones

--- app/src/main/java/edu/iesam/meceiot/features/sensorpanels/presentation/adapter/SensorViewHolder.kt
+++ app/src/main/java/edu/iesam/meceiot/features/sensorpanels/presentation/adapter/SensorViewHolder.kt
@@ -13,17 +13,17 @@ class SensorViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         itemSensor.apply {
             sensorName.text = result.name
             when (result.name) {
-                "Temp [ºC]" -> sensorName.setIconResource(R.drawable.ic_thermostat)
-                "Hum [%]" -> sensorName.setIconResource(R.drawable.ic_humidity)
-                "CO2 [ppm]" -> sensorName.setIconResource(R.drawable.ic_co2)
-                "Luz [lux]" -> sensorName.setIconResource(R.drawable.ic_light)
-                "Movimiento" -> sensorName.setIconResource(R.drawable.ic_motion)
-                "Bat [mV]" -> sensorName.setIconResource(R.drawable.ic_battery)
-                "Sound Avg [dB]" -> sensorName.setIconResource(R.drawable.ic_sound)
-                "Sound Max [dB]" -> sensorName.setIconResource(R.drawable.ic_sound)
-                else -> sensorName.setIconResource(R.drawable.ic_generic_sensor)
+                "Temp [ºC]" -> sensorIcon.setImageResource(R.drawable.ic_thermostat)
+                "Hum [%]" -> sensorIcon.setImageResource(R.drawable.ic_humidity)
+                "CO2 [ppm]" -> sensorIcon.setImageResource(R.drawable.ic_co2)
+                "Luz [lux]" -> sensorIcon.setImageResource(R.drawable.ic_light)
+                "Movimiento" -> sensorIcon.setImageResource(R.drawable.ic_motion)
+                "Bat [mV]" -> sensorIcon.setImageResource(R.drawable.ic_battery)
+                "Sound Avg [dB]" -> sensorIcon.setImageResource(R.drawable.ic_sound)
+                "Sound Max [dB]" -> sensorIcon.setImageResource(R.drawable.ic_sound)
+                else -> sensorIcon.setImageResource(R.drawable.ic_generic_sensor)
             }
-            sensorName.setOnClickListener {
+            view.setOnClickListener {
                 onClickListener(
                     result.name,
                     result.panelName,

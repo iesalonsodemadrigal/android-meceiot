447951130d499161606df56c2a2c2dd934d0e310
Chema <josem.perram@educa.jcyl.es>
2025-04-11 09:19:43 +0200

    [release] bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,13 +15,13 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )

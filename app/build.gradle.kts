5aa9df7c6e0795fb2f4fe3863826e4e443a7a72e
Chema <josem.perram@educa.jcyl.es>
2025-04-13 11:47:18 +0200

    [fix] Solucionado problemas con la ofuscación

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "edu.iesam.meceiot"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

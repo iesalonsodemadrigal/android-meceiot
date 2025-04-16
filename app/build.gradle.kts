721fc11ab89325123b09a0ce90da6e51094a596b
Chema <josem.perram@educa.jcyl.es>
2025-04-16 07:07:34 +0200

    [release] bump version (#122)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "edu.iesam.meceiot"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
